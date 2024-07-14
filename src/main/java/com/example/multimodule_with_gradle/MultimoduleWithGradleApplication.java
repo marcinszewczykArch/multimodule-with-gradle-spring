e16a3f7dd78362de393fab7ea05b75e8f1b90a5e
marcin.szewczyk <marcin.szewczyk@ocado.com>
2024-07-14 20:35:45 +0200

    Initial commit

---
+++ src/main/java/com/example/multimodule_with_gradle/MultimoduleWithGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.multimodule_with_gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultimoduleWithGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultimoduleWithGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/multimodule_with_gradle/MultimoduleWithGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multimodule_with_gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultimoduleWithGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
